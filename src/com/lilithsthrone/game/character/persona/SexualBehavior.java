82c1d963200044266b28157094c95c67c0bbbc84
Miles_Light <sirlighteater@gmail.com>
2019-08-29 14:15:11 +0200

    Added enum class in order to give game characters different behaviors during sex, depending on their personnalities.

---
+++ src/com/lilithsthrone/game/character/persona/SexualBehavior.java
@@ -0,0 +1,36 @@
+package com.lilithsthrone.game.character.persona;
+
+/***
+ * @since 0.4.1
+ * @version 0.4.1
+ * @author Miles_Light
+ */
+
+public enum SexualBehavior {
+	
+	//All PT are average
+	PLAIN,
+	
+//----------------------------------------Introvert sub-types----------------------------------------------------------------------------------------
+
+	SHY, 		// Pure introvert
+	FREAK,		//Introvert + Curious
+	COLD,		//Introvert + Cautious
+	CUTE,		//Introvert + Agreable
+	JUDGING,	//Introvert + Selfish
+	SAPIO, 		//Introvert + Confidence
+	LSE,		//Introvert + Neurotic, stands for Low self esteem
+	
+	
+//--------------------------------------Extravert sub-types-----------------------------------------------------------------------------------------------
+	
+	COCKY, 		//Pure extrovert
+	RISKY, 		//Extrovert + Curious, temp name 
+	WORRIED,	//Extrovert + Cautious,
+	CARING, 	//Extrovert + Agreable
+	DEGRADING, 	//Extrovert + Selfish
+	LEADING,	//Extrovert + Confidence
+	AW 			//Extrovert + Neurotic, stands for Attention Whore
+	
+
+}
